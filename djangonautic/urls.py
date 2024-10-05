21bf5c3a76fe50f595dc0252553da83aac8fca18
haseebzeeshan <haseebzeeshan2010@gmail.com>
2024-10-05 18:04:13 +0100

    first commit

---
+++ djangonautic/urls.py
@@ -0,0 +1,14 @@
+"""
+"""
+
+from django.contrib import admin
+from django.urls import path
+
+from blog.views import homepage
+from blog.views import about
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("about/", about),
+    path("", homepage),
+]
